3a4bed3e06bb5f651fb1f7a07433e652e7833f44
username <user email>
2022-12-28 14:44:53 +0300

    Fixed add and edit page, added first tests

--- spec/system/articles_spec.rb
+++ spec/system/articles_spec.rb
@@ -1,15 +1,72 @@
 require 'rails_helper'
 
 RSpec.describe "Articles", type: :system do
-  describe 'GET /' do
-    before { get root_path } 
-  
-    it 'returns http success' do
-      expect(response).to have_http_status(:success)
+=begin
+  describe 'root in russian' do
+
+    it 'page has a button' do
+      visit root_path(locale: :ru)
+      expect(page).to have_link("Создай свой персональный туториал")
     end
-  
-    it 'responds with html' do
-      expect(response.content_type).to match(%r{text/html})
+
+    it 'changes locale from rus to eng' do
+      visit root_path(locale: :ru)
+      click_on 'Английский'
+      expect(page).to have_current_path(root_path(locale: :en))
     end
+
+    it 'redirects to authorization' do
+      visit root_path(locale: :ru)
+      click_on 'Создай свой персональный туториал'
+      expect(page).to have_current_path(new_user_session_path(locale: :ru))
+    end
+
+    it 'redirects to authorization' do
+      visit root_path(locale: :ru)
+      click_on 'Создай свой персональный туториал'
+      expect(page).to have_current_path(new_user_session_path(locale: :ru))
+    end
+
+    it 'redirects to authorization' do
+      visit root_path(locale: :ru)
+      click_on 'Вход'
+      expect(page).to have_current_path(new_user_session_path(locale: :ru))
+    end
+
   end
+
+  describe 'root in english' do
+
+    it 'page has a button' do
+      visit root_path(locale: :en)
+      expect(page).to have_link("Create your own tutorial")
+    end
+
+    it 'changes locale from eng to rus' do
+      visit root_path(locale: :en)
+      click_on 'Russian'
+      expect(page).to have_current_path(root_path(locale: :ru))
+    end
+
+    it 'redirects to authorization' do
+      visit root_path(locale: :en)
+      click_on 'Create your own tutorial'
+      expect(page).to have_current_path(new_user_session_path(locale: :en))
+    end
+
+    it 'redirects to authorization' do
+      visit root_path(locale: :en)
+      click_on 'Create your own tutorial'
+      expect(page).to have_current_path(new_user_session_path(locale: :en))
+    end
+
+  end
+=end
+  describe 'creating new article' do
+  #  visit new_article_path(locale: :ru)
+   # fill_in('title', :with => 'Rspec')
+  # click_button('submit')
+  #  expect(page).to have_current_path(root_path(locale: :ru))
+  end
+
 end
