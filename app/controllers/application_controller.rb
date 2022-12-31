621530918f015c844d33ad9fa941be0e5d260c48
username <user email>
2022-12-31 23:18:14 +0300

    Before rubocop 2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,27 +1,29 @@
+# frozen_string_literal: true
+
+# base controller
 class ApplicationController < ActionController::Base
-    before_action :configure_permitted_parameters, if: :devise_controller?
-    before_action :set_locale
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_locale
 
-    protected
-  
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-      devise_parameter_sanitizer.permit(:account_update, keys: [:name])
-    end
+  protected
 
-    def default_url_options
-      {locale: I18n.locale}
-    end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
 
-    def set_locale
-      I18n.locale = extract_locale || I18n.default_locale
-    end
+  def default_url_options
+    { locale: I18n.locale }
+  end
 
-    def extract_locale
-      parsed_locale = params[:locale]
-      I18n.available_locales.map(&:to_s).include?(parsed_locale) ?
-        parsed_locale.to_sym :
-        nil
-    end
+  def set_locale
+    I18n.locale = extract_locale || I18n.default_locale
+  end
 
+  def extract_locale
+    parsed_locale = params[:locale]
+    return unless I18n.available_locales.map(&:to_s).include?(parsed_locale)
+
+    parsed_locale.to_sym
+  end
 end
